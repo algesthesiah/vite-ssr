ef249a5ac81775122bc40713b745c1a546b309b0
luleo <541877028@qq.com>
2022-07-21 10:24:30 +0800

    fix: remove code

--- modules/common/index.page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from 'react-vant'
-import { t } from '@lingui/macro'
-
-export { Page }
-
-function Page() {
-  return <Button type="primary">{t`hello`}</Button>
-}
---
+++ modules/index.page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Button } from 'react-vant'
+import { t } from '@lingui/macro'
+
+export { Page }
+
+function Page() {
+  return (
+    <div>
+      {t`hello`}
+      {/* <Button type="primary">btn</Button> */}
+    </div>
+  )
+}
